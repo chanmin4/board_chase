eacbe6d9f4f608b1a2016a539054c9116e7a1575
Jong Min Lee <chanmin4@naver.com>
2026-03-22 21:46:22 +0900

    add gamemanager scene

---
+++ Assets/Scripts/GamePlay/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	[SerializeField] private GameStateSO _gameState = default;
+	//[Header("Broadcasting on")]
+
+	private void Start()
+	{
+		StartGame();
+	}
+
+	private void OnEnable()
+	{
+	}
+
+	private void OnDisable()
+	{
+	}
+
+	void StartGame()
+	{
+		_gameState.UpdateGameState(GameState.Gameplay);
+	}
+}
